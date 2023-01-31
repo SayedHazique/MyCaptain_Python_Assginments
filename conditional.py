e5c4a75db8b7121b91e5f313b7eaf6f5a461ac01
SayedHazique <123817748+SayedHazique@users.noreply.github.com>
2023-01-31 12:50:24 +0530

    Positive Numbers
    Python program to print all positive numbers in a range.

---
+++ conditional.py
@@ -0,0 +1,13 @@
+list1=[12, -7, 5, 64, -14]
+for number in list1:
+    while(number>0):
+        print("Numbers in list1: ", number)
+        break
+    
+print("\n")
+    
+list2=[12, 14, -95, 3]
+for number in list2:
+    while(number>0):
+        print("Numbers in list2: ", number)
+        break
